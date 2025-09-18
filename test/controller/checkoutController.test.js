89753cdb92428866d0081973eb2a3db35388b05f
elemgraziela <elemgraziela@yahoo.com.br>
2025-09-18 08:24:25 -0300

    Adicionei teste para validar o controller

---
+++ test/controller/checkoutController.test.js
@@ -0,0 +1,38 @@
+const request = require('supertest');
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const app = require('../../rest/app');
+
+describe('Checkout Controller', () => {
+    describe('POST /api/checkout', () => {
+        it('Token inválido o retorno é 401', async () => {
+            const res = await request(app)
+                .post('/api/checkout')
+                .set('Authorization', 'Bearer token_invalido')
+                .send({
+                    items: [
+                        {
+                            productId: 0,
+                            quantity: 0
+                        }
+                    ],
+                    freight: 0,
+                    paymentMethod: "boleto",
+                    cardData: {
+                        number: "string",
+                        name: "string",
+                        expiry: "string",
+                        cvv: "string"
+                    }
+                });
+
+            expect(res.status).to.equal(401);
+            expect(res.body).to.have.property('error', 'Token inválido');
+        });
+    });
+
+    describe('GET /checkout/success', () => {
+
+    });
+});
